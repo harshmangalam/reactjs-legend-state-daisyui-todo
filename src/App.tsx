58c41e55d2187e58e656c6b3d32baa32295396d1
harshmangalam <harshdev8218@gmail.com>
2023-07-26 03:02:08 +0530

    added legend state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 export default function App() {
   return (
-    <div>
-      <button className="btn">Button</button>
+    <div className="min-h-screen h-full bg-base-200 py-8">
+      <section className="flex flex-col gap-y-2 items-center max-w-xl mx-auto text-center">
+        <h1 className="font-bold text-3xl">Todo</h1>
+        <p className="text-xl">
+          Todo web app build with React | Vite | Legend State | Tailwindcss |
+          Daisyui
+        </p>
+      </section>
+
+      <section className="mt-8"></section>
     </div>
   );
 }
